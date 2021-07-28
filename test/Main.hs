134b6d750e8a6b95e41c011c555ccfb25f000de0
Oleg Prutz <oleg@mlabs.city>
2021-07-28 04:36:13 +0300

    Unit tests for vault arithmetic functions

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import Test.Tasty (defaultMain, testGroup)
+import  Test.ArdanaDollar.Utils (vaultUnitTests)
 
 
 main :: IO ()
 main = defaultMain $ testGroup "tests"
-  []
+  [ vaultUnitTests ]
