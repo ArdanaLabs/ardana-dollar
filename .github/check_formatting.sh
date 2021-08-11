f11f7809e208a1431a82e47228ae82d8a1db8a84
Isaac Shapira <fresheyeball@protonmail.com>
2021-08-10 19:52:05 -0600

    more iteration to nixify

--- .github/check_formatting.sh
+++ .github/check_formatting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Extensions necessary to tell fourmolu about 
+# Extensions necessary to tell fourmolu about
 EXTENSIONS="-o -XTypeApplications -o -XTemplateHaskell -o -XImportQualifiedPost -o -XPatternSynonyms -o -fplugin=RecordDotPreprocessor"
 SOURCES=$(find -name '*.hs' -not -path './dist-*/*')
-~/.local/bin/fourmolu --mode check --check-idempotence $EXTENSIONS $SOURCES
+fourmolu --mode check --check-idempotence $EXTENSIONS $SOURCES
