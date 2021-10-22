a23566f7e89f38db265fd5e76577e9bd32dd157b
Fraser Dunlop <fraser@mlabs.city>
2021-10-22 11:51:40 +0100

    formatting

--- test/Test/Data/StickBreakingSet/Mock.hs
+++ test/Test/Data/StickBreakingSet/Mock.hs
@@ -1,24 +1,25 @@
 module Test.Data.StickBreakingSet.Mock (mockStickBreakingSetTests) where
-import qualified Data.Set as S
-import qualified Data.Map as M
-import Data.List (isPrefixOf)
-import Prelude
-import Data.Kind (Type)
+
 import Control.Monad (join)
+import Data.Kind (Type)
+import Data.List (isPrefixOf)
+import Data.Map qualified as M
+import Data.Set qualified as S
 import Hedgehog (
   Group (..),
-  Property,
   MonadGen,
-  property,
-  forAll,
+  Property,
   footnoteShow,
+  forAll,
+  property,
   (===),
  )
-import Hedgehog.Range qualified as Range
 import Hedgehog.Gen qualified as Gen
+import Hedgehog.Range qualified as Range
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Hedgehog (fromGroup)
-
+import Prelude
+import Safe (maximumMay)
 
 mockStickBreakingSetTests :: TestTree
 mockStickBreakingSetTests = testGroup "StickBreakingSet" [fromGroup $ Group "tests" [("is a set", mockSetIsASet)]]
@@ -26,16 +27,16 @@ mockStickBreakingSetTests = testGroup "StickBreakingSet" [fromGroup $ Group "tes
 -- this is a mock of the datum - BuiltinByteString would replace String on chain
 -- note there are no links between nodes so on-chain traversal is impossible by design
 -- what this set provides is efficient proof of absence, existence, and insertion (I'm thinking about deletion)
-data Stick =
-  Stick {
-    prefix :: String
-  , leafs  :: [String]
+data Stick = Stick
+  { prefix :: String
+  , leafs :: [String]
   , branches :: [String]
-  } deriving Show
+  }
+  deriving (Show)
 
 -- this is a mock of something that may exist off-chain for creating transactions and off-chain traversal
 newtype MockSet = MockSet (M.Map String Stick)
-  deriving Show
+  deriving (Show)
 
 mockEmpty :: MockSet
 mockEmpty = MockSet (M.singleton "" (Stick "" [] []))
@@ -43,7 +44,7 @@ mockEmpty = MockSet (M.singleton "" (Stick "" [] []))
 -- this is a proof that can be done on chain that consumes a single Stick and reproduces it
 mockContains :: MockSet -> String -> Bool
 mockContains (MockSet m) s =
-  let longestSharedPrefix = longest $ filter (`elem` M.keys m) ((s%) <$> M.keys m) 
+  let longestSharedPrefix = longest $ filter (`elem` M.keys m) ((s %) <$> M.keys m)
    in case M.lookup longestSharedPrefix m of
         Nothing -> error "map lookup error"
         Just so -> drop (length (prefix so)) s `elem` leafs so
@@ -55,31 +56,31 @@ mockContains (MockSet m) s =
 mockInsert :: MockSet -> String -> MockSet
 mockInsert ms s | mockContains ms s = ms
 mockInsert (MockSet m) s =
-  let longestSharedPrefix = longest $ filter (`elem` M.keys m) ((s%) <$> M.keys m)
-  in case M.lookup longestSharedPrefix m of
-       Nothing -> error "malformed set"
-       Just so ->
-         let wop = drop (length (prefix so)) s
-         in case longest ((wop%) <$> leafs so) of
-              "" ->
-                case longest ((wop%) <$> branches so) of
-                  -- The case where the insert goes into an existing node
-                  "" -> MockSet (M.insert longestSharedPrefix (so { leafs = drop (length longestSharedPrefix) s:leafs so }) m)
-                  e ->
-                  -- The case where a branch is broken
-                  --  - it is replaced with base of the branch containing the inserted node and the tip of the branch
-                    let rmd =  M.insert longestSharedPrefix (so { branches = e:filter (not . isPrefixOf e) (branches so)}) m
-                        np = longestSharedPrefix <> e
-                        w = head $ filter (isPrefixOf e) (branches so)
-                        in MockSet (M.insert np (Stick np [drop (length np) s] [drop (length e) w]) rmd)
-              e ->
-                -- The case where a leaf is broken
-                -- - it is deleted and the common prefix with the inserted element becomes a branch containing
-                --   the broken leaf and the inserted element
-                let rmd = M.insert longestSharedPrefix (so { leafs = filter (not . isPrefixOf e) (leafs so), branches = e:branches so}) m
-                    np = longestSharedPrefix <> e
-                    w = head $ filter (isPrefixOf e) (leafs so)
-                 in MockSet (M.insert np (Stick np [ drop (length e) w, drop (length np) s] []) rmd)
+  let longestSharedPrefix = longest $ filter (`elem` M.keys m) ((s %) <$> M.keys m)
+   in case M.lookup longestSharedPrefix m of
+        Nothing -> error "malformed set"
+        Just so ->
+          let wop = drop (length (prefix so)) s
+           in case longest ((wop %) <$> leafs so) of
+                "" ->
+                  case longest ((wop %) <$> branches so) of
+                    -- The case where the insert goes into an existing node
+                    "" -> MockSet (M.insert longestSharedPrefix (so {leafs = drop (length longestSharedPrefix) s : leafs so}) m)
+                    e ->
+                      -- The case where a branch is broken
+                      --  - it is replaced with base of the branch containing the inserted node and the tip of the branch
+                      let rmd = M.insert longestSharedPrefix (so {branches = e : filter (not . isPrefixOf e) (branches so)}) m
+                          np = longestSharedPrefix <> e
+                          w = head $ filter (isPrefixOf e) (branches so)
+                       in MockSet (M.insert np (Stick np [drop (length np) s] [drop (length e) w]) rmd)
+                e ->
+                  -- The case where a leaf is broken
+                  -- - it is deleted and the common prefix with the inserted element becomes a branch containing
+                  --   the broken leaf and the inserted element
+                  let rmd = M.insert longestSharedPrefix (so {leafs = filter (not . isPrefixOf e) (leafs so), branches = e : branches so}) m
+                      np = longestSharedPrefix <> e
+                      w = head $ filter (isPrefixOf e) (leafs so)
+                   in MockSet (M.insert np (Stick np [drop (length e) w, drop (length np) s] []) rmd)
 
 ---- this requires traversal and is not possible to do on chain. it's here for testing
 --mockSize :: MockSet -> Int
@@ -87,7 +88,7 @@ mockInsert (MockSet m) s =
 
 -- also for testing
 mockAsList :: MockSet -> [String]
-mockAsList (MockSet m) = join [ (p <>) <$> ls | Stick p ls _ <- snd <$> M.toList m]
+mockAsList (MockSet m) = join [(p <>) <$> ls | Stick p ls _ <- snd <$> M.toList m]
 
 mockFromSet :: S.Set String -> MockSet
 mockFromSet s = foldr (flip mockInsert) mockEmpty $ S.toList s
@@ -100,7 +101,6 @@ genSetOfStrings = do
   l <- Gen.list (Range.linear 0 100) (Gen.string (Range.singleton 32) Gen.alphaNum)
   pure $ S.fromList l
 
-
 mockSetIsASet :: Property
 mockSetIsASet =
   property $ do
@@ -109,14 +109,12 @@ mockSetIsASet =
     footnoteShow mock
     s === S.fromList (mockAsList mock)
 
-
 -- helpers
 
 -- oh god, what sorcery is this?!? it's code golf for shared prefix of a String e.g.  "apple" % "apply" = "appl"
 (%) :: String -> String -> String
-(c:x)%(d:y)|c==d=c:x%y;_%_=""
+(c : x) % (d : y) | c == d = c : x % y; _ % _ = ""
 
 longest :: [String] -> String
-longest xss = case [(length xs, xs) | xs <- xss] of
-                [] -> ""
-                el -> snd $ maximum el
+longest xss = maybe "" snd (maximumMay [(length xs, xs) | xs <- xss])
+
