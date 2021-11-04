2d2e2a9536f4b3dd987f9f07cc7c53eed84472e1
Las Safin <me@las.rs>
2021-11-04 16:56:52 +0000

    Print size of nodeValidPolicy

--- util/Sizes.hs
+++ util/Sizes.hs
@@ -1,11 +1,20 @@
 import ArdanaDollar.Vault (vaultValidator)
-import Plutus.V1.Ledger.Api (PubKeyHash(PubKeyHash))
-import Plutus.V1.Ledger.Scripts (Validator(Validator))
+import ArdanaDollar.Map.Types (MapInstance(MapInstance))
+import ArdanaDollar.Map.ValidatorsTH (nodeValidPolicy)
+import Plutus.V1.Ledger.Value (AssetClass(AssetClass))
+import Plutus.V1.Ledger.Api (CurrencySymbol(CurrencySymbol), TokenName(TokenName), PubKeyHash(PubKeyHash))
+import Plutus.V1.Ledger.Scripts (Validator(Validator), MintingPolicy(MintingPolicy))
 import Data.Aeson qualified as Aeson
 import Data.Maybe (fromJust)
 import Prelude
 import Codec.Serialise (serialise)
 import Data.ByteString.Lazy qualified as B
+
+dummyAssetClass :: AssetClass
+dummyAssetClass = AssetClass
+  ( CurrencySymbol . fromJust . Aeson.decode $ "\"42E9FCF913093DFA0246DD743804E7AD437207119B0027ECD3401BB7496880E6\""
+  , TokenName . fromJust . Aeson.decode $ "\"42E9FCF913093DFA0246DD743804E7AD437207119B0027ECD3401BB7496880E6\""
+  )
 
 dummyPubKeyHash :: PubKeyHash
 dummyPubKeyHash = PubKeyHash . fromJust . Aeson.decode $ "\"DCA286992118F6BC911E8FED01E731A5EFF18C9914FC8E73AF55923DE8FD711D\""
@@ -13,5 +22,6 @@ dummyPubKeyHash = PubKeyHash . fromJust . Aeson.decode $ "\"DCA286992118F6BC911E
 main :: IO ()
 main = do
   let (Validator vs) = vaultValidator dummyPubKeyHash
-  let vss = serialise vs
-  putStrLn $ "vaultValidator: " <> show (B.length vss)
+  let (MintingPolicy ns) = nodeValidPolicy (MapInstance dummyAssetClass)
+  putStrLn $ "vaultValidator: " <> (show . B.length . serialise $ vs)
+  putStrLn $ "nodeValidPolicy: " <> (show . B.length . serialise $ ns)
